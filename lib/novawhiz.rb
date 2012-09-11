9487f89497ad94b096173db88e668e063e0e84b1
Tim Miller <tim.miller.0@gmail.com>
2012-09-11 21:26:49 +0000

    boot an instance and run commands

--- lib/novawhiz.rb
+++ lib/novawhiz.rb
@@ -1,8 +1,90 @@
+require 'openstack'
+require 'net/ssh/simple'
+class NovaWhiz
 
-module NovaWhiz
+  attr_accessor :os
 
-  def self.foo
-    'bar'
+  def initialize(opts)
+    @os = OpenStack::Connection.create(
+      :username => opts[:username],
+      :api_key => opts[:password],
+      :authtenant => opts[:authtenant],
+      :auth_url => opts[:auth_url],
+      :service_type => "compute")
+  end
+
+  def flavor_id(name)
+    flavors = @os.flavors.select { |f| f[:name] == name }
+    raise "ambiguous/unknown flavor: #{name}" unless flavors.length == 1
+    flavors.first[:id]
+  end
+
+  def image_id(reg)
+    images = @os.images.select { |i| i[:name] =~ reg }
+    raise "ambiguous/unknown image: #{reg} : #{images.inspect}" unless images.length == 1
+    images.first[:id]
+  end
+
+  def new_key(name)
+    key = @os.create_keypair :name => name
+    key[:private_key]
+  end
+
+  def public_ip(server)
+    server.accessipv4
+  end
+
+  def wait(timeout, interval=10)
+    while timeout > 0 do
+      return if yield
+      sleep interval
+      timeout -= interval
+    end
+  end
+
+  def run_command(creds, cmd)
+    Net::SSH::Simple.sync do
+      r = ssh(
+        creds[:ip],
+        cmd,
+        :user => creds[:user],
+        :key_data => [ creds[:key] ])
+      if !r.success
+        "command failed on #{creds[:ip]} : #{cmd} : #{r.stdout} : #{r.stderr}"
+      end
+      puts r.stdout
+      puts r.stderr
+    end
+  end
+
+  # boot an instance and return creds
+  def boot(opts)
+    opts[:flavor] ||= 'standard.xsmall'
+    opts[:image]  ||= /Ubuntu Precise/
+    opts[:sec_groups] ||= ['default']
+    opts[:key_name] ||= 'default'
+    raise 'no name provided' if !opts[:name] or opts[:name].empty?
+
+    private_key = new_key opts[:name]
+    server = @os.create_server(
+      :imageRef => image_id(opts[:image]),
+      :flavorRef => flavor_id(opts[:flavor]),
+      :key_name => opts[:name],
+      :security_groups => opts[:sec_groups],
+      :name => opts[:name])
+
+    wait(300) do
+      server = @os.server(server.id)
+      raise 'error booting vm' if server.status == 'ERROR'
+      server.status == 'ACTIVE'
+    end
+    sleep 60
+
+    {
+      :ip => public_ip(server),
+      :user => 'ubuntu',
+      :key => private_key
+    }
   end
 
 end
--- novawhiz.gemspec
+++ novawhiz.gemspec
@@ -1,12 +1,14 @@
 Gem::Specification.new do |s|
   s.name        = 'novawhiz'
-  s.version     = '0.0.0'
+  s.version     = '0.0.1'
   s.date        = '2012-09-11'
   s.summary     = 'simplify nova operations'
   s.description = 'library and command line tool for simplifying openstack nova operations'
   s.authors     = ['tim miller']
   s.email       = 'none@example.com'
   s.files       = ["lib/novawhiz.rb"]
-  s.homepage    =
-    'http://rubygems.org/gems/novawhiz'
+  s.homepage    = 'http://rubygems.org/gems/novawhiz'
+
+  s.add_runtime_dependency 'openstack',      '>= 0'
+  s.add_runtime_dependency 'net-ssh-simple', '>= 0'
 end
